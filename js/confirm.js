586f3ff9ebc10e029f749585584fc44ee6ed52a4
Candice <wuqiong910718@163.com>
2018-02-26 10:21:55 +0800

    解决网页在iPhone中撑开的问题

--- js/confirm.js
+++ js/confirm.js
@@ -6,4 +6,13 @@ $(function () {
   historyBack = function() {
     window.history.back();
   };
+
+  // v4.判断手机平台 
+  function judgePlatform() {
+    var platform = navigator.platform;
+    if (platform == "iPhone") {
+      $('#confirm_iframe').attr("scrolling","no");
+    }
+  };
+  judgePlatform();
 });
